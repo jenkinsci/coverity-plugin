d97d7610db4394ac80040df439d502a554f788b9
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-20 14:16:47 -0800

    work in progress told

---
+++ src/test/java/jenkins/plugins/coverity/CoverityPublisherTest.java
@@ -0,0 +1,24 @@
+package jenkins.plugins.coverity;
+
+import hudson.model.FreeStyleProject;
+import junit.framework.TestCase;
+import org.jvnet.hudson.test.HudsonTestCase;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class CoverityPublisherTest extends HudsonTestCase {
+    /**
+     * Work in progress configuration roundtrip testing.
+     */
+    public void testFoo() throws Exception {
+//        FreeStyleProject p = createFreeStyleProject();
+//        CoverityPublisher before = new CoverityPublisher("foo", null, "bar", "zot", true, null, null);
+//        p.getPublishersList().add(before);
+//
+//        configRoundtrip(p);
+//        CoverityPublisher after = p.getPublishersList().get(CoverityPublisher.class);
+//
+//        assertEqualDataBoundBeans(before,after);
+    }
+}
