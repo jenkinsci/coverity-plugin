aa4df1d988acaf4f4dea1e4c39487a09a1a0bca0
David Dooley <ddooley@coverity.com>
2016-07-13 10:25:06 -0400

    Fix the NullPointerException relating to the WAR feature in BZ 66004. Without this the feature simply doesnt work.

--- src/main/java/jenkins/plugins/coverity/analysis/PreFresnoToolHandler.java
+++ src/main/java/jenkins/plugins/coverity/analysis/PreFresnoToolHandler.java
@@ -54,7 +54,7 @@ public class PreFresnoToolHandler extends CoverityToolHandler {
 
         // If WAR files specified, emit them prior to running analysis
         // Do not check for presence of Java streams or Java in build
-        List<String> warFiles = null;
+        List<String> warFiles = new ArrayList<String>();
         if(invocationAssistance != null){
             List<String> givenWarFiles = invocationAssistance.getJavaWarFilesNames();
             if(givenWarFiles != null && !givenWarFiles.isEmpty()){
