b12ee4615be23960f8b19202b372be21bd95e099
Franco Rossi <frossi@synopsys.com>
2016-07-29 13:01:44 -0700

    Fix:84567 Adding trust store once more by reverting changes on commit: 507bd15de9d9a02a5a3a006902053f828ac4d64f

--- src/main/java/jenkins/plugins/coverity/SSLConfigurations.java
+++ src/main/java/jenkins/plugins/coverity/SSLConfigurations.java
@@ -1,6 +1,10 @@
 package jenkins.plugins.coverity;
 
+import com.coverity.truststore.SslConnector;
+import com.coverity.truststore.url.HttpsTrustStoreStreamHandlerFactory;
+import com.coverity.truststore.url.HttpsTrustStoreUrlConnection;
 import net.sf.json.JSONObject;
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class SSLConfigurations {
@@ -13,12 +17,12 @@ public class SSLConfigurations {
     public SSLConfigurations(boolean trustNewSelfSignedCert, JSONObject certFileJSON){
         setTrustNewSelfSignedCert(trustNewSelfSignedCert);
 
-        /*SslConfigSingleton singleton = SslConfigSingleton.getInstance();
+        SslConfigSingleton singleton = SslConfigSingleton.getInstance();
         if(trustNewSelfSignedCert){
             singleton.setOnNewSelfSignedCert(SslConnector.OnNewSelfSignedCert.valueOf("trust"));
         } else {
             singleton.setOnNewSelfSignedCert(SslConnector.OnNewSelfSignedCert.valueOf("distrust"));
-        }*/
+        }
 
         String certFileName = null;
         if(certFileJSON != null){
@@ -26,8 +30,7 @@ public class SSLConfigurations {
         }
 
         if(certFileName != null && !certFileName.isEmpty()){
-            //singleton.setCertFileName(certFileName);
-            setCertFileName(certFileName);
+            singleton.setCertFile(certFileName);
         }
     }
 
@@ -58,20 +61,20 @@ public class SSLConfigurations {
     /**
      * Singleton used for configurations for ssl in case ssl is selected.
      */
-    /*private static class SslConfigSingleton {
+    private static class SslConfigSingleton {
 
-        *//**
+        /**
          * Specify whether to trust or not a self-signed certificate.
-         *//*
-        //private SslConnector.OnNewSelfSignedCert onNewSelfSignedCert;
+         */
+        private SslConnector.OnNewSelfSignedCert onNewSelfSignedCert;
 
         public void setOnNewSelfSignedCert(SslConnector.OnNewSelfSignedCert onNewSelfSignedCert) {
             this.onNewSelfSignedCert = onNewSelfSignedCert;
         }
 
-        *//**
+        /**
          * Sets default extra trust store path.
-         *//*
+         */
         public void setCertFile(String certFileName) {
             HttpsTrustStoreUrlConnection.setDefaultExtraTrustStorePath(certFileName);
         }
@@ -99,6 +102,6 @@ public class SSLConfigurations {
             }
             return instance;
         }
-    }*/
+    }
 
 }
