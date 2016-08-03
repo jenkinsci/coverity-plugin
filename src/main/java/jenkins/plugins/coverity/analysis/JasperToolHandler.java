894c1f90ce339e23932395b67f465160d7c51711
Franco Rossi <frossi@synopsys.com>
2016-08-03 11:25:47 -0700

    Removing all trust store related changes.

--- src/main/java/jenkins/plugins/coverity/analysis/JasperToolHandler.java
+++ src/main/java/jenkins/plugins/coverity/analysis/JasperToolHandler.java
@@ -40,14 +40,6 @@ public class JasperToolHandler extends CoverityToolHandler{
         TaOptionBlock testAnalysis = publisher.getTaOptionBlock();
         ScmOptionBlock scm = publisher.getScmOptionBlock();
 
-        boolean isTrustNewSelfSignedCert = false;
-        String certFileName = null;
-        SSLConfigurations sslConfigurations = publisher.getDescriptor().getSslConfigurations();
-        if(sslConfigurations != null){
-            isTrustNewSelfSignedCert = sslConfigurations.isTrustNewSelfSignedCert();
-            certFileName = sslConfigurations.getCertFileName();
-        }
-
         // Seting the new envVars after jenkins has modified its own
         CoverityUtils.setEnvVars(envVars);
 
@@ -180,7 +172,10 @@ public class JasperToolHandler extends CoverityToolHandler{
                     cmd.add("--dir");
                     cmd.add(temp.getTempDir().getRemote());
                     cmd.addAll(testAnalysis.getTaCommandArgs());
-                    cmd.addAll(EnvParser.tokenize(testAnalysis.getCustomTestCommand()));
+
+                    for(String arg : testAnalysis.getCustomTestCommand().split(" ")) {
+                        cmd.add(arg);
+                    }
 
                     // Evaluation the cmd to replace any evironment variables
                     cmd = CoverityUtils.evaluateEnvVars(cmd, envVars);
@@ -238,14 +233,6 @@ public class JasperToolHandler extends CoverityToolHandler{
                         cmd.add(cimStream.getStream());
                         if(cim.isUseSSL()){
                             cmd.add("--ssl");
-                            if(isTrustNewSelfSignedCert){
-                                cmd.add("--on-new-cert");
-                                cmd.add("trust");
-                            }
-                            if(certFileName != null){
-                                cmd.add("--certs");
-                                cmd.add(certFileName);
-                            }
                         }
 
                         cmd.add("--user");
@@ -336,7 +323,9 @@ public class JasperToolHandler extends CoverityToolHandler{
                 }
 
                 if(scm.getScmAdditionalCmd() != null) {
-                    cmd.addAll(EnvParser.tokenize(scm.getScmAdditionalCmd()));
+                    for(String arg : scm.getScmAdditionalCmd().trim().replaceAll(" +", " ").split(" ")){
+                        cmd.add(arg);
+                    }
                 }
 
                 // Evaluation the cmd to replace any evironment variables
@@ -435,7 +424,9 @@ public class JasperToolHandler extends CoverityToolHandler{
                 }
 
                 if(effectiveIA.getAnalyzeArguments() != null && !isMisraAnalysis) {
-                    cmd.addAll(EnvParser.tokenize(effectiveIA.getAnalyzeArguments()));
+                    for(String arg : effectiveIA.getAnalyzeArguments().trim().replaceAll(" +", " ").split(" ")){
+                        cmd.add(arg);
+                    }
                 }
 
                 cmd = CoverityUtils.evaluateEnvVars(cmd, envVars);
@@ -544,14 +535,6 @@ public class JasperToolHandler extends CoverityToolHandler{
                         cmd.add("--https-port");
                         cmd.add(Integer.toString(cim.getPort()));
                         cmd.add("--ssl");
-                        if(isTrustNewSelfSignedCert){
-                            cmd.add("--on-new-cert");
-                            cmd.add("trust");
-                        }
-                        if(certFileName != null){
-                            cmd.add("--certs");
-                            cmd.add(certFileName);
-                        }
                     }else{
                         cmd.add("--port");
                         cmd.add(Integer.toString(cim.getPort()));
@@ -564,7 +547,9 @@ public class JasperToolHandler extends CoverityToolHandler{
 
                     if(invocationAssistance != null){
                         if(effectiveIA.getCommitArguments() != null) {
-                            cmd.addAll(EnvParser.tokenize(effectiveIA.getCommitArguments()));
+                            for(String arg : effectiveIA.getCommitArguments().split(" ")) {
+                                cmd.add(arg);
+                            }
                         }
                     }
 
