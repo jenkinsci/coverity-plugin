ce3826810d9095612ff554ef86d1ee28e327f34f
Andrew Cho <acho@synopsys.com>
2017-06-06 08:21:25 -0600

    Fix some test violations for the "CheckConfig" class.

--- src/main/java/jenkins/plugins/coverity/CheckConfig.java
+++ src/main/java/jenkins/plugins/coverity/CheckConfig.java
@@ -18,7 +18,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import org.jenkinsci.remoting.RoleChecker;
 
