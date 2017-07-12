8a941f6fca3de4a4de896690bd801b227439a881
Joel Briggs <jbriggs@synopsys.com>
2017-07-12 11:52:20 -0600

    Added more tests for CoverityViewResultsDescriptor

--- src/test/java/jenkins/plugins/coverity/CoverityViewResultsDescriptorTest.java
+++ src/test/java/jenkins/plugins/coverity/CoverityViewResultsDescriptorTest.java
@@ -13,7 +13,6 @@ package jenkins.plugins.coverity;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -33,17 +32,16 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
 
 import hudson.util.FormValidation;
 import hudson.util.FormValidation.Kind;
+import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import jenkins.plugins.coverity.CoverityPublisher.DescriptorImpl;
 import jenkins.plugins.coverity.ws.TestWebServiceFactory;
 import jenkins.plugins.coverity.ws.TestWebServiceFactory.TestConfigurationService;
 import jenkins.plugins.coverity.ws.TestableViewsService;
 import jenkins.plugins.coverity.ws.WebServiceFactory;
-
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Jenkins.class, WebServiceFactory.class, Client.class})
@@ -86,6 +84,17 @@ public class CoverityViewResultsDescriptorTest {
     }
 
     @Test
+    public void doFillInstanceItems_returnsGlobalPublisherIntances() {
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        final ListBoxModel result = descriptor.doFillConnectInstanceItems();
+
+        assertEquals(1, result.size());
+        assertEquals(cimInstance.getName(), result.get(0).name);
+        assertEquals(cimInstance.getName(), result.get(0).value);
+    }
+
+    @Test
     public void doCheckViews_nullValuesRequired() {
         final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
 
@@ -110,13 +119,27 @@ public class CoverityViewResultsDescriptorTest {
     }
 
     @Test
-    public void doCheck_withInvalidConnectInstance() {
+    public void doCheck_withUnknownConnectInstance() {
         final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
 
         FormValidation result = descriptor.doCheckViews("unknown-instance", "projectId", "viewName");
 
         assertEquals(Kind.ERROR, result.kind);
         assertThat(result.toString(), CoreMatchers.containsString("Unable to find Coverity Connect instance: unknown-instance"));
+    }
+
+    @Test
+    public void doCheck_withInvalidConnectInstance() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        // set up the user to have no permissions at all
+        testConfigurationService.setupUser(cimInstance.getUser(), false, new HashMap<String, String[]>());
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "projectId", "viewName");
+
+        assertEquals(Kind.ERROR, result.kind);
+        assertThat(StringEscapeUtils.unescapeHtml(result.toString()),
+            CoreMatchers.containsString("\"" + cimInstance.getUser() + "\" does not have following permission(s): \"Access web services\""));
     }
 
     @Test
@@ -130,5 +153,185 @@ public class CoverityViewResultsDescriptorTest {
         assertEquals(Kind.ERROR, result.kind);
         assertThat(StringEscapeUtils.unescapeHtml(result.toString()),
             CoreMatchers.containsString("Unable to find project 'projectId' on instance '" + cimInstance.getName() + "'."));
+    }
+
+    @Test
+    public void doCheck_withInvalidProjectId_suggestsKnownProjects() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        testConfigurationService.setupProjects("pipeline-project", 2, "s", 1);
+        TestableViewsService.setupWithViewApi(StringUtils.EMPTY);
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "projectId", "viewName");
+
+        assertEquals(Kind.ERROR, result.kind);
+        final String errorMessage = StringEscapeUtils.unescapeHtml(result.toString());
+        assertThat(errorMessage,
+            CoreMatchers.containsString("Unable to find project 'projectId' on instance '" + cimInstance.getName() + "'."));
+        assertThat(errorMessage,
+            CoreMatchers.containsString("Available projects include: pipeline-project0, pipeline-project1"));
+    }
+
+    @Test
+    public void doCheck_withInvalidConnectView() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        testConfigurationService.setupProjects("pipeline-project", 1, "s", 1);
+        TestableViewsService.setupWithViewApi(StringUtils.EMPTY);
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "pipeline-project0", "viewName");
+
+        assertEquals(Kind.ERROR, result.kind);
+        assertThat(StringEscapeUtils.unescapeHtml(result.toString()),
+            CoreMatchers.containsString("Unable to find the view 'viewName' on instance '" + cimInstance.getName() + "'."));
+    }
+
+    @Test
+    public void doCheck_withInvalidConnectView_suggestsKnownViews() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        testConfigurationService.setupProjects("pipeline-project", 1, "s", 1);
+        final String viewApiJsonResult = "{\"views\": [" +
+            "    {" +
+            "        \"id\": 54321," +
+            "        \"type\": \"issues\"," +
+            "        \"name\": \"Custom View\"," +
+            "        \"groupBy\":false," +
+            "        \"columns\": [" +
+            "            {" +
+            "                \"name\": \"cid\"," +
+            "                \"label\": \"CID\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"checker\"," +
+            "                \"label\": \"Checker\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFunction\"," +
+            "                \"label\": \"Function\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFile\"," +
+            "                \"label\": \"File\"" +
+            "            }" +
+            "        ]" +
+            "    }" +
+            "    {" +
+            "        \"id\": 67890," +
+            "        \"type\": \"issues\"," +
+            "        \"name\": \"All Defects\"," +
+            "        \"groupBy\":false," +
+            "        \"columns\": [" +
+            "            {" +
+            "                \"name\": \"cid\"," +
+            "                \"label\": \"CID\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayType\"," +
+            "                \"label\": \"Type\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayImpact\"," +
+            "                \"label\": \"Impact\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"firstDetected\"," +
+            "                \"label\": \"First Detected\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"owner\"," +
+            "                \"label\": \"Owner\"" +
+            "            }       " +
+            "        ]" +
+            "    }" +
+            "]}";
+        TestableViewsService.setupWithViewApi(viewApiJsonResult);
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "pipeline-project0", "viewName");
+
+        assertEquals(Kind.ERROR, result.kind);
+        final String errorMessage = StringEscapeUtils.unescapeHtml(result.toString());
+        assertThat(errorMessage,
+            CoreMatchers.containsString("Unable to find the view 'viewName' on instance '" + cimInstance.getName() + "'."));
+        assertThat(errorMessage,
+            CoreMatchers.containsString("Available views include: Custom View, All Defects"));
+    }
+
+    @Test
+    public void doCheck_withValidConfiguration() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        testConfigurationService.setupProjects("pipeline-project", 1, "s", 1);
+        final String viewApiJsonResult = "{\"views\": [" +
+            "    {" +
+            "        \"id\": 54321," +
+            "        \"type\": \"issues\"," +
+            "        \"name\": \"Custom View\"," +
+            "        \"groupBy\":false," +
+            "        \"columns\": [" +
+            "            {" +
+            "                \"name\": \"cid\"," +
+            "                \"label\": \"CID\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"checker\"," +
+            "                \"label\": \"Checker\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFunction\"," +
+            "                \"label\": \"Function\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFile\"," +
+            "                \"label\": \"File\"" +
+            "            }" +
+            "        ]" +
+            "    }" +
+            "]}";
+        TestableViewsService.setupWithViewApi(viewApiJsonResult);
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "pipeline-project0", "Custom View");
+
+        assertEquals(Kind.OK, result.kind);
+        assertEquals("Successfully configured", result.getMessage());
+    }
+
+    @Test
+    public void doCheck_withValidConfiguration_usingConnectViewId() {
+        final TestConfigurationService testConfigurationService = setupConfigurationService(cimInstance);
+        testConfigurationService.setupProjects("pipeline-project", 1, "s", 1);
+        final String viewApiJsonResult = "{\"views\": [" +
+            "    {" +
+            "        \"id\": 54321," +
+            "        \"type\": \"issues\"," +
+            "        \"name\": \"Custom View\"," +
+            "        \"groupBy\":false," +
+            "        \"columns\": [" +
+            "            {" +
+            "                \"name\": \"cid\"," +
+            "                \"label\": \"CID\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"checker\"," +
+            "                \"label\": \"Checker\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFunction\"," +
+            "                \"label\": \"Function\"" +
+            "            }," +
+            "            {" +
+            "                \"name\": \"displayFile\"," +
+            "                \"label\": \"File\"" +
+            "            }" +
+            "        ]" +
+            "    }" +
+            "]}";
+        TestableViewsService.setupWithViewApi(viewApiJsonResult);
+        final CoverityViewResultsDescriptor descriptor = new CoverityViewResultsDescriptor();
+
+        FormValidation result = descriptor.doCheckViews(cimInstance.getName(), "pipeline-project0", "54321");
+
+        assertEquals(Kind.OK, result.kind);
+        assertEquals("Successfully configured", result.getMessage());
     }
 }
